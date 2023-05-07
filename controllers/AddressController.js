04ae77085eec661aa9b26e111e45cae3cbae2e93
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-05-07 18:03:10 -0400

    Adjusted the data to send to the frontend

--- controllers/AddressController.js
+++ controllers/AddressController.js
@@ -6,10 +6,11 @@ const GetCensusTract = async (req, res, next) => {
 		const city = req.params.city.replace('%20', '+');
 		const state = req.params.state.replace('%20', '+');
 		const zipCode = req.params.zipCode.replace('%20', '+');
+		// res.send('success');
 		const results = await axios.get(
 			`https://geocoding.geo.census.gov/geocoder/geographies/address?street=${streetAddress}&city=${city}&state=${state}&zip=${zipCode}&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&format=json`
 		);
-		res.status(200).json(results);
+		res.status(200).json(results.data);
 	} catch (error) {
 		return res.status(500).json({ error: error.message });
 	}
