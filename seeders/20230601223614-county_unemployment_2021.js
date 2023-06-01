eb4bd38132bb646f2b69d2d9bdf9072d7f0a943c
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-01 18:46:06 -0400

    Seeded tables with unemployment data by county for 2021 and 2022

---
+++ seeders/20230601223614-county_unemployment_2021.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const csv = require('csv-parser');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const seedData = [];
+
+		return new Promise((resolve, reject) => {
+			fs.createReadStream('data/county_unemployment_2021.csv')
+				.pipe(csv())
+				.on('data', (row) => {
+					seedData.push({
+						...row,
+						createdAt: new Date(),
+						updatedAt: new Date()
+					}); // Push each row of the CSV into the seedData array
+				})
+				.on('end', async () => {
+					try {
+						await queryInterface.bulkInsert(
+							'county_unemployment_2021',
+							seedData,
+							{}
+						);
+						resolve();
+					} catch (error) {
+						reject(error);
+					}
+				});
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		queryInterface.bulkDelete('county_unemployment_2021');
+	}
+};
