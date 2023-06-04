cb36593558e0c15c16dfbabdfd0d194452aa839a
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-06-04 19:24:42 -0400

    Set up brownfield site model and migration

---
+++ migrations/20230604230547-create-brownfield-sites.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('brownfield_sites', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			cleanup_name: {
+				type: Sequelize.STRING
+			},
+			location_address: {
+				type: Sequelize.STRING
+			},
+			city_name: {
+				type: Sequelize.STRING
+			},
+			state_code: {
+				type: Sequelize.STRING
+			},
+			postal_code: {
+				type: Sequelize.STRING
+			},
+			county_name: {
+				type: Sequelize.STRING
+			},
+			epa_id: {
+				type: Sequelize.STRING
+			},
+			latitude: {
+				type: Sequelize.STRING
+			},
+			longitude: {
+				type: Sequelize.STRING
+			},
+			epa_region_code: {
+				type: Sequelize.STRING
+			},
+			tribal_land: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('brownfield_sites');
+	}
+};
---
+++ models/brownfieldsites.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class BrownfieldSites extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	BrownfieldSites.init(
+		{
+			cleanup_name: DataTypes.STRING,
+			location_address: DataTypes.STRING,
+			city_name: DataTypes.STRING,
+			state_code: DataTypes.STRING,
+			postal_code: DataTypes.STRING,
+			county_name: DataTypes.STRING,
+			epa_id: DataTypes.STRING,
+			latitude: DataTypes.STRING,
+			longitude: DataTypes.STRING,
+			epa_region_code: DataTypes.STRING,
+			tribal_land: DataTypes.STRING
+		},
+		{
+			sequelize,
+			modelName: 'BrownfieldSites',
+			tableName: 'brownfield_sites'
+		}
+	);
+	return BrownfieldSites;
+};
