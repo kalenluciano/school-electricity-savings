4f490b1d8a203a9b9620c957795509a44381a128
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-09-10 14:05:23 -0400

    Create solar_geo_battery_stats table migration

---
+++ migrations/20230910180232-create-solar-geo-battery-stats.js
@@ -0,0 +1,80 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('solar_geo_battery_stats', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			total_tax_credit: {
+				type: Sequelize.INTEGER,
+			},
+			base_credit: {
+				type: Sequelize.INTEGER,
+				defaultValue: 30,
+			},
+			additional_credit: {
+				type: Sequelize.INTEGER,
+			},
+			low_income_status: {
+				type: Sequelize.BOOLEAN,
+			},
+			census_tract_poverty_rate: {
+				type: Sequelize.INTEGER,
+			},
+			census_tract_family_median_income: {
+				type: Sequelize.INTEGER,
+			},
+			metro_area_family_median_income: {
+				type: Sequelize.INTEGER,
+			},
+			state_family_median_income: {
+				type: Sequelize.INTEGER,
+			},
+			indian_land_status: {
+				type: Sequelize.BOOLEAN,
+			},
+			federal_american_indian_reservation: {
+				type: Sequelize.BOOLEAN,
+			},
+			off_reservation_trust_land: {
+				type: Sequelize.BOOLEAN,
+			},
+			alaska_native_village: {
+				type: Sequelize.BOOLEAN,
+			},
+			majority_tribe_members: {
+				type: Sequelize.BOOLEAN,
+			},
+			coal_mine_status: {
+				type: Sequelize.BOOLEAN,
+			},
+			fossil_fuel_employment_status: {
+				type: Sequelize.BOOLEAN,
+			},
+			msa_non_msa_name: {
+				type: Sequelize.STRING,
+			},
+			msa_non_msa_unemployment_rate: {
+				type: Sequelize.INTEGER,
+			},
+			brownfield_site_status: {
+				type: Sequelize.BOOLEAN,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('solar_geo_battery_stats');
+	},
+};
---
+++ models/solargeobatterystats.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class SolarGeoBatteryStats extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	SolarGeoBatteryStats.init(
+		{
+			total_tax_credit: DataTypes.INTEGER,
+			base_credit: { type: DataTypes.INTEGER, defaultValue: 30 },
+			additional_credit: DataTypes.INTEGER,
+			low_income_status: DataTypes.BOOLEAN,
+			census_tract_poverty_rate: DataTypes.INTEGER,
+			census_tract_family_median_income: DataTypes.INTEGER,
+			metro_area_family_median_income: DataTypes.INTEGER,
+			state_family_median_income: DataTypes.INTEGER,
+			indian_land_status: DataTypes.BOOLEAN,
+			federal_american_indian_reservation: DataTypes.BOOLEAN,
+			off_reservation_trust_land: DataTypes.BOOLEAN,
+			alaska_native_village: DataTypes.BOOLEAN,
+			majority_tribe_members: DataTypes.BOOLEAN,
+			coal_mine_status: DataTypes.BOOLEAN,
+			fossil_fuel_employment_status: DataTypes.BOOLEAN,
+			msa_non_msa_name: DataTypes.STRING,
+			msa_non_msa_unemployment_rate: DataTypes.INTEGER,
+			brownfield_site_status: DataTypes.BOOLEAN,
+		},
+		{
+			sequelize,
+			modelName: 'SolarGeoBatteryStats',
+			tableName: 'solar_geo_battery_stats',
+		}
+	);
+	return SolarGeoBatteryStats;
+};
