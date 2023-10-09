215b02229a8c9a6be82229855e757a84076228aa
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-08 21:06:22 -0400

    Removed outdated/unnecessary data and controllers

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,6 @@
 const Router = require('express').Router();
 const CalculatorRouter = require('./CalculatorRouter');
-const SavingsRouter = require('./SavingsRouter');
 
 Router.use('/address', CalculatorRouter);
-Router.use('/savings', SavingsRouter);
 
 module.exports = Router;
--- routes/SavingsRouter.js
+++
@@ -1,6 +0,0 @@
-const Router = require('express').Router();
-const SavingsController = require('../controllers/SavingsController');
-
-Router.get('/', SavingsController.GetSavingsBaseline);
-
-module.exports = Router;
